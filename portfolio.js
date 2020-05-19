7d6b8375d9b18792053f2a27b196d86aa9a5b1a7
alexbeausire <30601391+alexbeausire@users.noreply.github.com>
2020-05-19 16:45:51 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -7,7 +7,7 @@ backgroundImage[0] = "images/image1.png";
 backgroundImage[1] = "images/image2.png";
 backgroundImage[2] = "images/image3.png";
 backgroundImage[3] = "images/image4.jpg";
-backgroundImage[4] = "images/image5.jpg";
+backgroundImage[4] = "images/027Artboard 1@4x.png";
 
 function displayAllImages() {
     for (i=1;i<backgroundImage.length;i++) {

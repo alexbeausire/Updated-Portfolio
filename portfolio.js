c4e1574ec85d101b2dfb12f9252de9e6849b2a62
alexbeausire <30601391+alexbeausire@users.noreply.github.com>
2020-05-19 15:47:31 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+console.log("Hello")
+var baseUrl = "/images/";
+var pictureIndex = 0;
+var pictures = [];
+var backgroundImage = new Array(); 
+backgroundImage[0] = "images/image1.png";
+backgroundImage[1] = "images/image2.png";
+backgroundImage[2] = "images/image3.png";
+backgroundImage[3] = "images/image4.jpg";
+backgroundImage[4] = "images/image5.jpg";
+
+function displayAllImages() {
+    for (i=1;i<backgroundImage.length;i++) {
+        document.write("<div class='imglist'><img src=" + backgroundImage[i] + "></div>");
+    };
+};
